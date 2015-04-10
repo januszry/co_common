18c8d2672419a1b65cdb8069c1f2fef6c75b33f6
root <root@hztmp.(none)>
2015-04-10 21:58:30 +0800

    [audio/id3taggen.py] function: add tag to file

--- cocommon/audio/id3taggen.py
+++ cocommon/audio/id3taggen.py
@@ -4,6 +4,7 @@ A Simple TextOnly ID3Tag (v2.4.0) Generator
 
 import struct
 import functools
+import os
 
 import hexdump
 
@@ -13,6 +14,7 @@ encoding_dict = {'iso-8859-1': 0, 'utf-16': 1, 'utf-16be': 2, 'utf-8': 3}
 
 
 class ID3Tag(object):
+    """ID3Tag Generator."""
 
     def __init__(self, flag=0x00):
         self._flag = flag
@@ -36,17 +38,36 @@ class ID3Tag(object):
     @property
     def tag(self):
         return self.header + functools.reduce(
-                lambda x, y: x + y, map(lambda z: z.frame, self._frames), b'')
+            lambda x, y: x + y, map(lambda z: z.frame, self._frames), b'')
 
     def add_frame(self, frame_id, desc, value, encoding='utf-8', flag=0x0000):
+        """Add a new ID3Frame to this ID3Tag.
+
+        :param: frame_id: Frame ID like TXXX
+        :param: desc: description
+        :param: value: value
+        :param: encoding: utf-8 | utf-16 | utf-16be | iso-8859-1
+        :param: flag: 16bit flag"""
         self._frames.append(ID3Frame(frame_id, desc, value, encoding, flag))
 
     def write(self, fn):
+        """Write to a binary file only containing ID3Tag."""
         with open(fn, 'wb') as f:
             f.write(self.tag)
 
+    def add_to_adts_file(self, fn, new_fn=None):
+        """Add ID3v2 tag to ADTS file.
+
+        Note that ADTS files actually do not support metadata,
+        thus this may produce problematic files.
+
+        Note that this function manipulates binary directly,
+        file already containing tags should not be provided as input."""
+        add_id3tag_to_adts(fn, tag=self.tag, output_adts_file=new_fn)
+
 
 class ID3Frame(object):
+    """An ID3v2 Frame."""
 
     def __init__(self, frame_id, desc, value,
                  encoding='utf-8', flag=0x0000):
@@ -68,9 +89,45 @@ class ID3Frame(object):
     @property
     def payload(self):
         return struct.pack('>b', encoding_dict[self._encoding]) + \
-                '{desc}\0{value}\0'.format(
-                desc=self._desc, value=self._value).encode(self._encoding)
+            '{desc}\0{value}\0'.format(
+            desc=self._desc, value=self._value).encode(self._encoding)
 
     @property
     def frame(self):
         return self.header + self.payload
+
+
+def add_id3tag_to_adts(adts_file, output_adts_file=None,
+                       tag=None, tag_file=None):
+    """Add ID3v2 tag to ADTS file.
+
+    Note that ADTS files actually do not support metadata,
+    thus this may produce problematic files.
+
+    Note that this function manipulates binary directly,
+    file already containing tags should not be provided as input."""
+    DROP_FIRST_N_FRAMES = 2
+    with open(adts_file, 'rb') as f:
+        origin = f.read()
+
+    if not tag and (not tag_file or not os.path.isfile(tag_file)):
+        raise Exception("Please provide at least one of tag and tag_file")
+
+    if not tag:
+        with open(tag_file, 'rb') as f:
+            tag = f.read()
+
+    index = 0
+    for i in range(DROP_FIRST_N_FRAMES):
+        index = origin.find(b'\xff\xf1', index + 1)
+
+    if output_adts_file is None:
+        output_adts_file = adts_file + '.tagged'
+    with open(output_adts_file, 'wb') as f:
+        f.write(origin[:index])
+        f.write(tag)
+        f.write(origin[index:])
+
+
+def remove_id3tag_from_adts(adts_file, output_adts_file=None):
+    raise Exception("Unimplemented")
--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="cocommon",
-        version="0.9.2",
-        packages=find_packages(),
-        zip_safe=False,
+    name="cocommon",
+    version="0.9.3",
+    packages=find_packages(),
+    zip_safe=False,
 
-        description="Common Utils and Tricks",
-        long_description="Common Utils and Tricks",
-        author="coppla",
-        author_email="januszry@gmail.com",
+    description="Common Utils and Tricks",
+    long_description="Common Utils and Tricks",
+    author="coppla",
+    author_email="januszry@gmail.com",
 
-        license="GPL",
-        keywords=("utils"),
-        platforms="Independant",
-        url="",
-        entry_points={'console_scripts': [
-            'auprober=cocommon.audio.auprober:main',
-            'wavanalyze=cocommon.audio.wavfile:main',
-            ]},
+    license="GPL",
+    keywords=("utils"),
+    platforms="Independant",
+    url="",
+    entry_points={'console_scripts': [
+        'auprober=cocommon.audio.auprober:main',
+        'wavanalyze=cocommon.audio.wavfile:main',
+        ]},
 
-        install_requires=["requests>=2.3.0",
-                          "pexpect>=3.3",
-                          "ujson>=1.33",
-                          "hexdump>=3.2"])
+    install_requires=["requests>=2.3.0",
+                      "pexpect>=3.3",
+                      "ujson>=1.33",
+                      "hexdump>=3.2"])
