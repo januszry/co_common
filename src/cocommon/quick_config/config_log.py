65225d992ca67e9e5df7d708eae1d2f8e6bcf8f2
zengruoyu <zengruoyu@qingtingfm.com>
2015-09-25 13:25:09 +0800

    minor fix

--- src/cocommon/quick_config/config_log.py
+++ src/cocommon/quick_config/config_log.py
@@ -6,7 +6,7 @@ import logging
 import logging.handlers
 
 
-def config_log(log_dir, log_file, log_level='INFO',
+def config_log(log_dir=None, log_file=None, log_level='INFO',
                rotate=True, back_count=7, name="",
                enable_stream_handler=True,
                multithreading=False, multiprocessing=False,
@@ -15,8 +15,10 @@ def config_log(log_dir, log_file, log_level='INFO',
 
     Will always create log files rotated by day.
 
-    :param log_dir: log directory
-    :param log_file: basename of log file under log_dir
+    :param log_dir: log directory.
+        If None, current dir
+    :param log_file: basename of log file under log_dir.
+        If None, no logging to file
     :param log_level: DEBUG | INFO | WARNING | ERROR | CRITICAL
     :param rotate: boolean to enable log rotation
     :param back_count: number of rotated logs to keep
@@ -26,7 +28,6 @@ def config_log(log_dir, log_file, log_level='INFO',
     :param multiprocessing: add processId and processName in log format
     :param log_format: if not None, use this format rather than default format
     """
-    log_file = os.path.join(log_dir, log_file)
 
     if log_format is None:
         log_format = '[%(levelname)s]'
@@ -37,11 +38,17 @@ def config_log(log_dir, log_file, log_level='INFO',
         log_format += '<%(module)s>-%(funcName)s: %(message)s --- %(asctime)s'
         log_formatter = logging.Formatter(log_format)
 
-    if rotate:
-        loghandler_file = logging.handlers.TimedRotatingFileHandler(
-            log_file, when='midnight', interval=1, backupCount=back_count)
-    else:
-        loghandler_file = logging.FileHandler(log_file)
+    if log_dir is None:
+        log_dir = '.'
+
+    if log_file is not None:
+        log_file = os.path.join(log_dir, log_file)
+        if rotate:
+            loghandler_file = logging.handlers.TimedRotatingFileHandler(
+                log_file, when='midnight', interval=1, backupCount=back_count)
+        else:
+            loghandler_file = logging.FileHandler(log_file)
+
     loghandler_file.setFormatter(log_formatter)
     loghandler_file.setLevel(getattr(logging, log_level.upper(), None))
 
